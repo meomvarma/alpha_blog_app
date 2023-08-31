79382d2ddcc6b33b0349e7308b4f0bb899efcc7f
meomvarma <om.varma@ordwaylabs.com>
2023-08-31 12:00:01 +0530

    Add one-to-many association between users and articles from backend

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-
+    belongs_to :user
     validates :title , presence:true , length: {minimum: 6, maximum: 15}
     validates :description , presence:true , length:{minimum: 8 , maximum: 200}
 
