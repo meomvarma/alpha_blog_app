c09d4fc47c92c8988d690ebfccc9a6344c43dc96
meomvarma <om.varma@ordwaylabs.com>
2023-09-01 11:14:42 +0530

    User signup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(article_params)
+        @article = Article.new(article_params) 
         @article.user = User.first
         if @article.save
             flash[:article] = "Article is created successfully"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, except: [:new]
   root 'pages#home'
   get 'about' ,  to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
 
 end
 
