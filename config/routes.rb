9645ad1b17bd539c1eede4b860b047f84f880626
meomvarma <om.varma@ordwaylabs.com>
2023-09-11 19:50:43 +0530

    Create and destroy user session

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   root 'pages#home'
   get 'about' ,  to: 'pages#about'
+
   resources :articles
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 end
 
